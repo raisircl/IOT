616ee53054b0c185922eebb27b6752895290cd39
Rai Singh <rai.verma@live.com>
2022-04-15 10:24:40 +0530

    c# classes start

---
+++ ConsoleAppVinodVerma/ConsoleAppVinodVerma/Program.cs
@@ -0,0 +1,22 @@
+﻿using System; // namespace - A logical name which is given to set of classes.
+using RaiLib;
+namespace ConsoleAppVinodVerma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyMaths m1 = new MyMaths();
+            int r=m1.sum(330, 20);
+            Console.WriteLine("Hello World!");
+            Console.WriteLine($"The Sum is {r}");
+
+            //Console.WriteLine("The sum is {0}", r);
+            
+            //string result = string.Format("The sum is {0}", r);
+            //Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ RaiLib/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RaiLib
+{
+    public class MyMaths
+    {
+        public int sum(int n1,int n2)
+        {
+            return n1 + n2;
+        }
+    }
+}
